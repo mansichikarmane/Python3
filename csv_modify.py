e294a20584e6dba072146c7680489ea59e0b95f9
mansichikarmane <mansi.chikarmane@gmail.com>
2021-07-28 21:41:50 -0700

    made script that sends file to s3 bucket

--- csv_modify.py
+++ csv_modify.py
@@ -1,16 +1,41 @@
 import sys
 import csv
+import argparse
+import boto3
+
+
+def get_args():
+  parser = argparse.ArgumentParser(description='Process command line values.')
+
+  parser.add_argument('--read-file', dest='readfile', required=True, help='file to read in')
+  parser.add_argument('--write-file', dest='writefile', required=True, help='file to write to')
+  parser.add_argument('--access-key', dest='accesskey', required=True, help='AWS access key')
+  parser.add_argument('--secret-key', dest='secretkey', required=True, help='AWS secret key')
+  parser.add_argument('--upload-bucket', dest='uploadbucket', required=True, help='bucket to upload file to in S3')
+  parser.add_argument('--file-path', dest='filepath', required=True, help='path of where to load the file')
+  parser.add_argument('--aws-service', dest='awsservice', required=False, default='s3', help='service to use in AWS')
+  
+  args = parser.parse_args()
+  return args
+
+
+def upload_to_s3(args):
+  client = boto3.client(args.awsservice, aws_access_key_id=args.accesskey, aws_secret_access_key=args.secretkey)
+  path = args.filepath + '/' + args.writefile
+  client.upload_file(args.writefile, args.uploadbucket, path)
 
 
 def main():
-  read_csv()
-  write_csv()
+  args = get_args()
+  read_csv(args)
+  write_csv(args)
+  upload_to_s3(args)
   
 
-def read_csv():
+def read_csv(args):
   print("Reading of CSV")
 
-  with open(sys.argv[1], newline='') as csvfile:
+  with open(args.readfile, newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
       print(', '.join(row))
@@ -18,12 +43,12 @@ def read_csv():
   print()
 
 
-def write_csv():
+def write_csv(args):
   list = generate_list()
 
   print("Write of CSV")
 
-  with open(sys.argv[2], 'w') as csvfile:
+  with open(args.writefile, 'w') as csvfile:
       writer = csv.writer(csvfile)
       
       for row in list:
